37fafcf835c5db1a032c1caf6c89cbe9d405ba54
Surohak <suren.hakobyan@picsart.com>
2020-02-24 10:51:30 +0400

    added concurrently

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,1 +1,1 @@
-export const apiBaseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : '';
+export const apiBaseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'http://my-json-server.typicode.com/Surohak/hostaway';
